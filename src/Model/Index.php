05a1b68f144abf6dd217131b0fee0040be5189c4
Julian Nuß <j.nuss@neusta.de>
2017-04-28 09:24:57 +0200

    Use env config in Index-Model
    Refactoring of Index-Model to use Magentos config (env) for host and
    port if nothing is given via constructor / di

--- src/Model/Index.php
+++ src/Model/Index.php
@@ -4,12 +4,15 @@
  *
  * Copyright (c) 2017 neusta GmbH | Ein team neusta Unternehmen
  *
- * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source
+ * code.
  *
  * @license https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause License
  */
 
 namespace TeamNeustaGmbh\Magentypo\Model;
+
+use Magento\Framework\App\DeploymentConfig;
 use Magento\Framework\ObjectManager\ObjectManager;
 
 /**
@@ -19,66 +22,118 @@ use Magento\Framework\ObjectManager\ObjectManager;
  */
 class Index
 {
-    const INDEX_NAME = "magentypo";
-    const PRODUCT_INDEX_NAME = "magentypo_products";
+    const INDEX_NAME = 'magentypo';
+    const PRODUCT_INDEX_NAME = 'magentypo_products';
+    const CATEGORY_INDEX_NAME = 'magentypo_categories';
 
-    const CATEGORY_INDEX_NAME = "magentypo_categories";
+    /**
+     * @var string
+     */
+    private $host;
+
+    /**
+     * @var int
+     */
+    private $port;
+
+    /**
+     * @var \Elastica\Index
+     */
+    private $index;
+
+    /**
+     * @var \Elastica\Client
+     */
+    private $client;
 
     /**
      * Index constructor.
+     *
+     * @param DeploymentConfig $config
+     * @param string|null      $host
+     * @param int|null         $port
+     *
+     * @throws \Elastica\Exception\InvalidException
+     * @throws \Elastica\Exception\ResponseException
      */
-    public function __construct(
-
-    )
+    public function __construct(DeploymentConfig $config, $host = null, $port = null)
     {
+        $this->host = $host ?? $config->get('elastic/default/host');
+        $this->port = $port ?? $config->get('elastic/default/port');
+
+        $this->_init();
+        $this->_createIndex();
+        $this->_createMapping();
+    }
+
+    /**
+     * Initialize elasticsearch client
+     */
+    private function _init()
+    {
+        /** @noinspection PhpUnnecessaryFullyQualifiedNameInspection */
         $this->client = new \Elastica\Client([
-            'host' => 'elasticsearch',
-            'port' => '9200'
+            'host' => $this->host,
+            'port' => $this->port,
         ]);
+    }
 
-        $this->index =  $this->client->getIndex(self::INDEX_NAME, true);
+    /**
+     * Create index on elasticsearch server
+     *
+     * @throws \Elastica\Exception\InvalidException
+     * @throws \Elastica\Exception\ResponseException
+     */
+    private function _createIndex()
+    {
+        $this->index = $this->client->getIndex(self::INDEX_NAME);
 
-        $this->index->create(
-            array(
-                'number_of_shards' => 4,
-                'number_of_replicas' => 1,
-                'analysis' => array(
-                    'analyzer' => array(
-                        'default_index' => array(
-                            'type' => 'custom',
-                            'tokenizer' => 'standard',
-                            'filter' => array('lowercase', 'mySnowball')
-                        ),
-                        'default_search' => array(
-                            'type' => 'custom',
-                            'tokenizer' => 'standard',
-                            'filter' => array('standard', 'lowercase', 'mySnowball')
-                        )
-                    ),
-                    'filter' => array(
-                        'mySnowball' => array(
-                            'type' => 'snowball',
-                            'language' => 'German'
-                        )
-                    )
-                )
-            ),
-            true
-        );
+        $indexConfig = [
+            'number_of_shards'   => 4,
+            'number_of_replicas' => 1,
+            'analysis'           => [
+                'analyzer' => [
+                    'default_index'  => [
+                        'type'      => 'custom',
+                        'tokenizer' => 'standard',
+                        'filter'    => ['lowercase', 'mySnowball'],
+                    ],
+                    'default_search' => [
+                        'type'      => 'custom',
+                        'tokenizer' => 'standard',
+                        'filter'    => ['standard', 'lowercase', 'mySnowball'],
+                    ],
+                ],
+                'filter'   => [
+                    'mySnowball' => [
+                        'type'     => 'snowball',
+                        'language' => 'German',
+                    ],
+                ],
+            ],
+        ];
 
+        $this->index->create($indexConfig, true);
+    }
+
+    /**
+     * Create mapping and send it to the elasticsearch server
+     */
+    private function _createMapping()
+    {
         $elasticaType = $this->index->getType(self::INDEX_NAME);
 
+        /** @noinspection PhpUnnecessaryFullyQualifiedNameInspection */
         $mapping = new \Elastica\Type\Mapping();
         $mapping->setType($elasticaType);
 
         $mapping->setProperties([
-            'id'      => ['type' => 'integer', 'include_in_all' => FALSE],
-            'name'    => ['type' => 'string', 'include_in_all' => TRUE],
-            'sku'     => ['type' => 'string', 'include_in_all' => TRUE]
+            'id'   => ['type' => 'integer', 'include_in_all' => false],
+            'name' => ['type' => 'string', 'include_in_all' => true],
+            'sku'  => ['type' => 'string', 'include_in_all' => true],
         ]);
 
-// Send mapping to type
+        // Send mapping to type
         $mapping->send();
-
     }
 }
